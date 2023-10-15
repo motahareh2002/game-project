503e7963d11d8ce646307f7bae95c61cd685d7bd
motahareh jafarian <motaharehjafi@gmail.com>
2023-10-15 21:46:24 +0330

    new

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -1,5 +1,11 @@
 <template>
     <div class="bg-[#181c23] w-full h-full min-h-screen">
+        <div>
+            <Icon name="uiw:facebook"/>
+            <Icon name="uiw:twitter"/>
+            <Icon name="bxl:youtube"/>
+            <Icon name="bxl:discord"/>
+        </div>
         <slot/>
     </div>
 </template>
